771c2499b73cee8f1bb9e7c9a8d64f1a1ef21d32
Avtar S <121425550+avtsingh@users.noreply.github.com>
2023-07-23 20:54:26 +0530

    Update sway.sh

--- sway.sh
+++ sway.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-sudo pacman -S neofetch neovim neovide btop firefox thunderbird mpv imv bc libreoffice-fresh chromium bleachbit mixxx inkscape gimp evince ly rsync xdg-user-dirs grim slurp wl-clipboard wf-recorder
+sudo pacman -S neofetch neovim neovide btop firefox thunderbird mpv imv bc libreoffice-fresh chromium bleachbit mixxx inkscape gimp evince ly rsync xdg-user-dirs wl-clipboard wf-recorder
 
 # Fonts
-sudo pacman -S ttf-font-awesome noto-fonts ttf-liberation adobe-source-code-pro-fonts awesome-terminal-fonts ttf-roboto ttf-roboto-mono ttf-mononoki-nerd ttf-meslo-nerd ttf-jetbrains-mono-nerd ttf-hack-nerd ttf-go-nerd ttf-firacode-nerd ttf-cascadia-code-nerd 
+sudo pacman -S ttf-font-awesome noto-fonts ttf-liberation awesome-terminal-fonts ttf-roboto ttf-roboto-mono ttf-mononoki-nerd ttf-meslo-nerd ttf-jetbrains-mono-nerd ttf-hack-nerd ttf-go-nerd ttf-firacode-nerd ttf-cascadia-code-nerd 
 
 # Printing
 
-sudo pacman -S cups ipp-usb cups-pdf libusb simple-scan
+sudo pacman -S cups ipp-usb cups-pdf simple-scan
 
 # Thunar
 sudo pacman -S thunar thunar-archive-plugin thunar-volman file-roller gvfs tumbler thunar-media-tags-plugin gtk3 gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb polkit-gnome
 
 # Sound
-sudo pacman -S gstreamer gst-libav gst-plugins-base gst-plugins-good gst-plugins-ugly gst-plugin-pipewire pipewire pipewire-alsa pipewire-audio pipewire-docs pipewire-ffado pipewire-jack pipewire-pulse pipewire-roc wireplumber
+sudo pacman -S gst-libav gst-plugins-base gst-plugins-good gst-plugins-ugly pipewire-docs pipewire-ffado pipewire-roc
 
 # system and network protocols
-sudo pacman -S acpi acpid avahi nftables firewalld networkmanager bluez dhclient dhcpcd dnsmasq iwd modemmanager network-manager-applet
+sudo pacman -S acpi acpid nftables firewalld bluez dhclient dhcpcd dnsmasq modemmanager
 
 # Create folders in user directory (eg. Documents,Downloads,etc.)
 xdg-user-dirs-update
